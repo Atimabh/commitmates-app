edf1d9fea66bc7035090353999a3b60fc1507032
atimabh <abarunaha@gmail.com>
2024-12-07 00:14:00 +0530

    commitment group ui

--- components/Commitments/CommitmentGroupDetails/index.tsx
+++ components/Commitments/CommitmentGroupDetails/index.tsx
@@ -1,10 +1,80 @@
-import styles from './styles'
-import { Text, View } from 'react-native'
+import { styles } from './styles'
+import { Feather } from '@expo/vector-icons'
+import { Pressable, SafeAreaView, ScrollView, Text, View } from 'react-native'
+import { ColorLibrary } from '@/constants/Colors'
+import { router } from 'expo-router'
+import { Image } from 'expo-image'
 
 export default function CommitmentGroupDetails() {
   return (
-    <View>
-      <Text>group details</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <View style={styles.header}>
+          <Pressable onPress={() => router.back()}>
+            <Feather name="chevron-left" size={28} color={ColorLibrary.primaryBlack} />
+          </Pressable>
+          <Pressable>
+            <Feather name="settings" size={24} color={ColorLibrary.primaryBlack} />
+          </Pressable>
+        </View>
+
+        <View style={styles.groupHeader}>
+          <View style={styles.groupInfo}>
+            <View>
+              <Text style={styles.groupName}>Lose 20 kgs</Text>
+              <Text style={styles.memberCount}>Jessica Smith • 2 Members</Text>
+              <View style={styles.frequencyContainer}>
+                <Feather name="calendar" size={20} color={ColorLibrary.lightGrey} />
+                <Text style={styles.frequency}>Frequency: Daily</Text>
+              </View>
+            </View>
+            <View style={styles.iconContainer}>
+              <Text style={styles.icon}>💪🏼</Text>
+            </View>
+          </View>
+          <View style={styles.buttons}>
+            <Pressable style={styles.followButton}>
+              <Feather name="plus-circle" size={20} color="#ffffff" />
+              <Text style={styles.followButtonText}>Proof</Text>
+            </Pressable>
+            <Pressable style={styles.shareButton}>
+              <Feather name="share" size={20} color={ColorLibrary.primaryBlack} />
+              <Text style={styles.shareButtonText}>Share</Text>
+            </Pressable>
+          </View>
+        </View>
+
+        <View style={styles.membersPreviewContainer}>
+          <View style={styles.members}>
+            <Image style={styles.memberDp} source={'https://i.pravatar.cc/100?img=52'} />
+            <Image style={[styles.memberDp, styles.overlap]} source={'https://i.pravatar.cc/100?img=53'} />
+            <Image style={[styles.memberDp, styles.overlap]} source={'https://i.pravatar.cc/100?img=54'} />
+          </View>
+          <Text style={styles.streakCount}>🔥 46</Text>
+        </View>
+
+        <View style={styles.sectionContainer}>
+          <Text style={styles.sectionName}>24 November 2024 - Today</Text>
+          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            <Pressable style={styles.postImageContainer}>
+              <Image style={styles.post} source="https://picsum.photos/150/150" />
+              <Image style={styles.postUserDp} source={'https://i.pravatar.cc/30?img=52'} />
+            </Pressable>
+            <Pressable style={styles.postImageContainer}>
+              <Image style={styles.post} source="https://picsum.photos/150/150" />
+              <Image style={styles.postUserDp} source={'https://i.pravatar.cc/30?img=53'} />
+            </Pressable>
+            <Pressable style={styles.postImageContainer}>
+              <Image style={styles.post} source="https://picsum.photos/150/150" />
+              <Image style={styles.postUserDp} source={'https://i.pravatar.cc/30?img=54'} />
+            </Pressable>
+            <Pressable style={styles.postImageContainer}>
+              <Image style={styles.post} source="https://picsum.photos/150/150" />
+              <Image style={styles.postUserDp} source={'https://i.pravatar.cc/30?img=55'} />
+            </Pressable>
+          </ScrollView>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
--- components/Commitments/CommitmentGroupDetails/styles.ts
+++ components/Commitments/CommitmentGroupDetails/styles.ts
@@ -1,3 +1,146 @@
+import { ColorLibrary } from '@/constants/Colors'
 import { StyleSheet } from 'react-native'
 
-export const styles = StyleSheet.create({})
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#ffffff',
+    flex: 1,
+  },
+  header: {
+    paddingHorizontal: 16,
+    paddingVertical: 24,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 12,
+  },
+  groupHeader: {
+    paddingHorizontal: 16,
+    paddingBottom: 16,
+    borderBottomColor: ColorLibrary.offWhite,
+    borderBottomWidth: 1,
+  },
+  groupInfo: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  groupName: {
+    fontFamily: 'DenverSerialHeavy',
+    fontSize: 26,
+    marginBottom: 8,
+    color: ColorLibrary.primaryBlack,
+  },
+  memberCount: {
+    fontFamily: 'InterMedium',
+    fontSize: 14,
+    color: ColorLibrary.lightGrey,
+    marginBottom: 18,
+  },
+  frequencyContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 6,
+  },
+  frequency: {
+    fontFamily: 'InterMedium',
+    fontSize: 14,
+    color: ColorLibrary.primaryBlack,
+  },
+  iconContainer: {
+    width: 68,
+    height: 68,
+    borderRadius: 100,
+    backgroundColor: ColorLibrary.offWhite,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  icon: {
+    fontSize: 26,
+  },
+  buttons: {
+    marginTop: 24,
+    flexDirection: 'row',
+    gap: 10,
+  },
+  followButton: {
+    backgroundColor: ColorLibrary.primaryBlack,
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 6,
+    borderRadius: 8,
+    paddingVertical: 10,
+  },
+  followButtonText: {
+    fontFamily: 'InterMedium',
+    fontSize: 14,
+    color: '#ffffff',
+  },
+  shareButton: {
+    backgroundColor: ColorLibrary.offWhite,
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 6,
+    borderRadius: 8,
+    paddingVertical: 10,
+  },
+  shareButtonText: {
+    fontFamily: 'InterMedium',
+    fontSize: 14,
+    color: ColorLibrary.primaryBlack,
+  },
+  membersPreviewContainer: {
+    marginTop: 24,
+    marginBottom: 24,
+    paddingHorizontal: 16,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  members: {
+    flexDirection: 'row',
+  },
+  memberDp: {
+    width: 28,
+    height: 28,
+    borderRadius: 100,
+  },
+  overlap: {
+    marginLeft: -6,
+  },
+  streakCount: {
+    fontFamily: 'InterSemiBold',
+    fontSize: 16,
+  },
+  sectionContainer: {
+    marginBottom: 24,
+    paddingLeft: 16,
+  },
+  sectionName: {
+    fontFamily: 'InterMedium',
+    fontSize: 14,
+    marginBottom: 16,
+  },
+  post: {
+    width: 116,
+    height: 116,
+    borderRadius: 6,
+    marginRight: 8,
+  },
+  postImageContainer: {
+    position: 'relative',
+  },
+  postUserDp: {
+    width: 24,
+    height: 24,
+    borderRadius: 100,
+    position: 'absolute',
+    bottom: 10,
+    left: 10,
+    borderWidth: 1.5,
+    borderColor: ColorLibrary.offWhite,
+  },
+})
